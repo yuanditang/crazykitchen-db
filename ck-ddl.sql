6197f075dc024362a06bd1e49b9bb25c0703470c
tantalum-73 <54928841+tantalum-73@users.noreply.github.com>
2024-08-14 16:29:23 -0400

    Add files via upload

--- ck-ddl.sql
+++ ck-ddl.sql
@@ -3,7 +3,7 @@ CustomerID INT PRIMARY KEY,
 Name VARCHAR(50),
 Email VARCHAR(50),
 Phone VARCHAR(15),
-MemberID INT
+MemberID INT FOREIGN KEY
 );
 
 CREATE TABLE Member (
@@ -33,7 +33,8 @@ Phone VARCHAR(15),
 Wage DECIMAL(10,2),
 ManagerID INT,
 Department VARCHAR(50),
-HireDate DATE
+HireDate DATE,
+FOREIGN KEY (ManagerID) REFERENCES Staff(StaffID)
 );
 
 CREATE TABLE RestaurantOrder (
@@ -41,14 +42,19 @@ OrderID INT PRIMARY KEY,
 OrderDatetime DATETIME,
 CustomerID INT,
 StaffID INT,
-TableNumber INT
+TableNumber INT,
+FOREIGN KEY (CustomerID) REFERENCES Customer(CustomerID), 
+FOREIGN KEY (StaffID) REFERENCES Staff(StaffID), 
+FOREIGN KEY (TableNumber) REFERENCES RestaurantTable(TableNumber) 
 );
 
 CREATE TABLE OrderedDish (
 OrderID INT,
 DishName VARCHAR(50),
 Quantity INT,
-PRIMARY KEY (OrderID, DishName)
+PRIMARY KEY (OrderID, DishName), 
+FOREIGN KEY (OrderID) REFERENCES RestaurantOrder(OrderID), 
+FOREIGN KEY (DishName) REFERENCES Dish(DishName) 
 );
 
 CREATE TABLE Supplier (
@@ -66,14 +72,18 @@ CREATE TABLE IngredientsUsed (
 IngredientID INT,
 DishName VARCHAR(50),
 Quantity DECIMAL(5,2),
-PRIMARY KEY (IngredientID, DishName)
+PRIMARY KEY (IngredientID, DishName),
+FOREIGN KEY (IngredientID) REFERENCES Ingredient(IngredientID), 
+FOREIGN KEY (DishName) REFERENCES Dish(DishName) 
 );
 
 CREATE TABLE IngredientsSupplied (
 IngredientID INT,
 SupplierID INT,
 Quantity DECIMAL(5,2),
-PRIMARY KEY (IngredientID, SupplierID)
+PRIMARY KEY (IngredientID, SupplierID),
+FOREIGN KEY (IngredientID) REFERENCES Ingredient(IngredientID), 
+FOREIGN KEY (SupplierID) REFERENCES Supplier(SupplierID) 
 );
 
 CREATE TABLE Receipt (
@@ -81,5 +91,6 @@ ReceiptID INT PRIMARY KEY,
 Amount DECIMAL(10,2),
 SaleDatetime DATETIME,
 PaymentMethod VARCHAR(20),
-OrderID INT
+OrderID INT,
+FOREIGN KEY (OrderID) REFERENCES RestaurantOrder(OrderID)
 );
